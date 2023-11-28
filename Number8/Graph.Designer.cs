ce0892153fc8ae3954e740d06f742e2af15aca97
Dylariz <zarec-k@mail.ru>
2023-11-28 12:18:36 +0300

    Init

---
+++ Number8/Graph.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Number8
+{
+    partial class Graph
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pinButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.pinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pinButton.Location = new System.Drawing.Point(857, 786);
+            this.pinButton.Name = "pinButton";
+            this.pinButton.Size = new System.Drawing.Size(412, 67);
+            this.pinButton.TabIndex = 0;
+            this.pinButton.Text = "Закрепить многоугольник";
+            this.pinButton.UseVisualStyleBackColor = true;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.statusLabel.Location = new System.Drawing.Point(399, 801);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(382, 43);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Ожидание построения";
+            // 
+            // Graph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1293, 883);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.pinButton);
+            this.Name = "Graph";
+            this.Text = "Graph";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Graph_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Graph_MouseDown);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button pinButton;
+        private System.Windows.Forms.Label statusLabel;
+
+        #endregion
+    }
+}
---
+++ Number8/Graph.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Number8
+{
+    public partial class Graph : Form
+    {
+        public Polygon Polygon { get; }
+        public Point XPoint { get; set; }
+        public bool IsPolygonFinished { get; set; }
+
+        public Graph()
+        {
+            InitializeComponent();
+            Polygon = new Polygon();
+            IsPolygonFinished = false;
+
+            Invalidate(); // Перерисовываем форму
+        }
+
+        private void Graph_Paint(object sender, PaintEventArgs e)
+        {
+            if (!IsPolygonFinished)
+            {
+                var points = Polygon.Points.ToArray();
+
+                e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+                if (points.Length >= 3)
+                    e.Graphics.DrawPolygon(Pens.Red, points.ToArray()); // Рисуем полигон по точкам из списка Points
+                else if (points.Length == 2)
+                    e.Graphics.DrawLine(Pens.Red, points[0], points[1]); // Рисуем линию по точкам из списка Points
+                else if (points.Length == 1)
+                    DrawPoint(points[0], e); // Рисуем точку
+            }
+
+            if (XPoint != Point.Empty) // Если точка XPoint задана, то рисуем её
+                e.Graphics.FillEllipse(Brushes.Red, XPoint.X - 2, XPoint.Y - 2, 3, 3);
+        }
+
+        private void Graph_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Мышь не наведена на элемент управления
+            if (e.Button == MouseButtons.Left)
+            {
+                if (IsPolygonFinished)
+                {
+                    XPoint = e.Location;
+                    statusLabel.Text = Polygon.IsPointInPolygon(XPoint) switch
+                    {
+                        1 => "Точка внутри многоугольника",
+                        -1 => "Точка снаружи многоугольника",
+                        _ => throw new ArgumentOutOfRangeException()
+                    };
+                        
+                }
+                else
+                    Polygon.AddPoint(e.Location);
+
+                Invalidate();
+            }
+        }
+
+        private void DrawPoint(Point point, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.Red, point.X - 2, point.Y - 2, 3, 3);
+        }
+
+        private void pinButton_Click(object sender, EventArgs e)
+        {
+            if (Polygon.IsValid)
+            {
+                IsPolygonFinished = true;
+                statusLabel.Text = "Ожидание точки X";
+            }
+            else
+            {
+                MessageBox.Show("Многоугольник не задан.");
+                Polygon.Clear();
+            }
+        }
+    }
+}
