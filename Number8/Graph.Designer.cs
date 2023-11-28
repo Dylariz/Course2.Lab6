6fbcec0170a3f7b885fb62535e767aa36bc128e3
Dylariz <zarec-k@mail.ru>
2023-11-28 21:27:26 +0300

    Complete program cycle

--- Number8/Graph.Designer.cs
+++ Number8/Graph.Designer.cs
@@ -35,10 +35,11 @@
             // 
             // pinButton
             // 
-            this.pinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.pinButton.Location = new System.Drawing.Point(12, 804);
+            this.pinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pinButton.Location = new System.Drawing.Point(23, 533);
+            this.pinButton.Margin = new System.Windows.Forms.Padding(2);
             this.pinButton.Name = "pinButton";
-            this.pinButton.Size = new System.Drawing.Size(412, 67);
+            this.pinButton.Size = new System.Drawing.Size(269, 41);
             this.pinButton.TabIndex = 0;
             this.pinButton.Text = "Закрепить многоугольник";
             this.pinButton.UseVisualStyleBackColor = true;
@@ -47,19 +48,22 @@
             // statusLabel
             // 
             this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.statusLabel.Location = new System.Drawing.Point(448, 810);
+            this.statusLabel.Location = new System.Drawing.Point(325, 533);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(679, 64);
+            this.statusLabel.Size = new System.Drawing.Size(339, 41);
             this.statusLabel.TabIndex = 1;
             this.statusLabel.Text = "Статус: Ожидание построения";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Graph
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1293, 883);
+            this.ClientSize = new System.Drawing.Size(824, 585);
             this.Controls.Add(this.statusLabel);
             this.Controls.Add(this.pinButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Graph";
             this.Text = "Graph";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Graph_Paint);
--- Number8/Graph.cs
+++ Number8/Graph.cs
@@ -4,71 +4,85 @@ using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Number8
+namespace Number8;
+
+public partial class Graph : Form
 {
-    public partial class Graph : Form
+    public Polygon Polygon { get; }
+    public Point XPoint { get; set; }
+    public bool IsPolygonFinished { get; set; }
+
+    public Graph()
     {
-        public Polygon Polygon { get; }
-        public Point XPoint { get; set; }
-        public bool IsPolygonFinished { get; set; }
+        InitializeComponent();
+        Polygon = new Polygon();
+        IsPolygonFinished = false;
+    }
 
-        public Graph()
+    private void Graph_Paint(object sender, PaintEventArgs e)
+    {
+        var points = Polygon.Points.ToArray();
+
+        e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+        if (points.Length >= 3)
+            e.Graphics.DrawPolygon(Pens.Red, points.ToArray()); // Рисуем полигон по точкам из списка Points
+        else if (points.Length == 2)
+            e.Graphics.DrawLine(Pens.Red, points[0], points[1]); // Рисуем линию по точкам из списка Points
+        else if (points.Length == 1)
+            e.Graphics.FillEllipse(Brushes.Red, points[0].X - 2, points[0].Y - 2, 3, 3);
+
+        if (XPoint != Point.Empty) // Если точка XPoint задана, то рисуем её
+            e.Graphics.FillEllipse(Brushes.Green, XPoint.X - 2, XPoint.Y - 2, 5, 5);
+    }
+    private void Graph_MouseDown(object sender, MouseEventArgs e)
+    {
+        // Мышь не наведена на элемент управления
+        if (e.Button == MouseButtons.Left)
         {
-            InitializeComponent();
-            Polygon = new Polygon();
-            IsPolygonFinished = false;
-        }
-
-        private void Graph_Paint(object sender, PaintEventArgs e)
-        {
-            var points = Polygon.Points.ToArray();
-
-            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
-            if (points.Length >= 3)
-                e.Graphics.DrawPolygon(Pens.Red, points.ToArray()); // Рисуем полигон по точкам из списка Points
-            else if (points.Length == 2)
-                e.Graphics.DrawLine(Pens.Red, points[0], points[1]); // Рисуем линию по точкам из списка Points
-            else if (points.Length == 1)
-                e.Graphics.FillEllipse(Brushes.Red, points[0].X - 2, points[0].Y - 2, 3, 3);
-
-            if (XPoint != Point.Empty) // Если точка XPoint задана, то рисуем её
-                e.Graphics.FillEllipse(Brushes.Green, XPoint.X - 2, XPoint.Y - 2, 4, 4);
-        }
-
-        private void Graph_MouseDown(object sender, MouseEventArgs e)
-        {
-            // Мышь не наведена на элемент управления
-            if (e.Button == MouseButtons.Left)
+            if (IsPolygonFinished)
             {
-                if (IsPolygonFinished)
+                XPoint = e.Location;
+                statusLabel.Text = Polygon.IsPointInPolygon(XPoint) switch
                 {
-                    XPoint = e.Location;
-                    statusLabel.Text = Polygon.IsPointInPolygon(XPoint) switch
-                    {
-                        1 => "Статус: Точка внутри многоугольника",
-                        -1 => "Статус: Точка снаружи многоугольника",
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
-                }
-                else
-                    Polygon.AddPoint(e.Location);
-
-                Invalidate();
-            }
-        }
-
-        private void pinButton_Click(object sender, EventArgs e)
-        {
-            if (Polygon.IsValid)
-            {
-                IsPolygonFinished = true;
-                statusLabel.Text = "Статус: Ожидание точки X";
+                    1 => "Статус: Точка внутри многоугольника",
+                    -1 => "Статус: Точка снаружи многоугольника",
+                    _ => throw new ArgumentOutOfRangeException()
+                };
             }
             else
-            {
-                MessageBox.Show("Многоугольник не задан.");
-                Polygon.Clear();
-            }
+                Polygon.AddPoint(e.Location);
+
+            Invalidate();
         }
+    }
+
+    private void pinButton_Click(object sender, EventArgs e)
+    {
+        if (IsPolygonFinished)
+        {
+            ResetToDefault();
+        }
+        else if (Polygon.IsValid)
+        {
+            IsPolygonFinished = true;
+            statusLabel.Text = "Статус: Ожидание точки X";
+            pinButton.Text = "Отчистить";
+        }
+        else
+        {
+            MessageBox.Show("Многоугольник не задан.");
+            Polygon.Clear();
+            Invalidate();
+        }
+    }
+
+    private void ResetToDefault()
+    {
+        IsPolygonFinished = false;
+        Polygon.Clear();
+        XPoint = Point.Empty;
+        statusLabel.Text = "Статус: Ожидание построения";
+        pinButton.Text = "Закрепить многоугольник";
+        Invalidate();
     }
 }
