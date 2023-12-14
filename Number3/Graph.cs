4b33b80b537dd7c3e99f78bdc0d5dd4562455218
Dylariz <zarec-k@mail.ru>
2023-12-14 15:32:33 +0300

    Add Number3

---
+++ Number3/Graph.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Number3;
+
+/// <summary>
+/// Самопересечение многоугольника.
+/// Многоугольник задан списком вершин в порядке обхода их по часовой стрелке.
+/// Самопересекающимся называется многоугольник, у которого хотя бы одна пара несмежных сторон имеет общую точку.
+/// Реализовать и протестировать метод, возвращающий истину, если многоугольник является самопересекающимся и ложь в противном случае.
+/// </summary>
+public partial class Graph : Form
+{
+    public Polygon Polygon { get; }
+    public Point XPoint { get; private set; }
+    public bool IsPolygonFinished { get; private set; }
+
+    public Graph()
+    {
+        InitializeComponent();
+        Polygon = new Polygon();
+        IsPolygonFinished = false;
+    }
+
+    private void Graph_Paint(object sender, PaintEventArgs e)
+    {
+        var points = Polygon.Points.ToArray();
+
+        e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+        if (points.Length >= 3)
+            e.Graphics.DrawPolygon(Pens.Red, points.ToArray()); // Рисуем полигон по точкам
+        else if (points.Length == 2)
+            e.Graphics.DrawLine(Pens.Red, points[0], points[1]); // Рисуем линию по точкам
+        else if (points.Length == 1)
+            e.Graphics.FillEllipse(Brushes.Red, points[0].X - 2, points[0].Y - 2, 3, 3); // Рисуем точку
+
+        if (XPoint != Point.Empty) // Если точка XPoint задана, то рисуем её
+            e.Graphics.FillEllipse(Brushes.Green, XPoint.X - 2, XPoint.Y - 2, 5, 5);
+    }
+    
+    private void Graph_MouseDown(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Left)
+        {
+
+            statusLabel.Text = Polygon.IsSelfIntersecting() switch
+            {
+                true => "Статус: Многоугольник самопересекающийся",
+                false => "Статус: Стороны многоугольника не пересекаются"
+            };
+        
+            Polygon.AddPoint(e.Location);
+            Invalidate();
+        }
+    }
+
+    private void ResetToDefault()
+    {
+        IsPolygonFinished = false;
+        Polygon.Clear();
+        XPoint = Point.Empty;
+        statusLabel.Text = "Статус: Ожидание построения";
+        pinButton.Text = "Закрепить многоугольник";
+        Invalidate();
+    }
+}
---
+++ Number3/Polygon.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Number3;
+
+public class Polygon
+{
+    private List<Point> _points = new();
+    
+    public IReadOnlyList<Point> Points => _points;
+    
+    public bool IsValid => Points.Count >= 3;
+
+    public void AddPoint(Point point)
+    {
+        _points.Add(point);
+    }
+    
+    public void Clear()
+    {
+        _points.Clear();
+    }
+    
+    public bool IsSelfIntersecting()
+    {
+        for (var i = 0; i < _points.Count - 1; i++)
+        {
+            for (var j = i + 2; j < _points.Count - 1; j++)
+            {
+                if (IsSegmentsIntersecting(_points[i], _points[i + 1], _points[j], _points[j + 1]))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+    
+    private static bool IsSegmentsIntersecting(Point a, Point b, Point c, Point d)
+    {
+        // Векторное произведение
+        var v1 = (d.X - c.X) * (a.Y - c.Y) - (d.Y - c.Y) * (a.X - c.X);
+        var v2 = (d.X - c.X) * (b.Y - c.Y) - (d.Y - c.Y) * (b.X - c.X);
+        var v3 = (b.X - a.X) * (c.Y - a.Y) - (b.Y - a.Y) * (c.X - a.X);
+        var v4 = (b.X - a.X) * (d.Y - a.Y) - (b.Y - a.Y) * (d.X - a.X);
+        return v1 * v2 < 0 && v3 * v4 < 0;
+    }
+}
